f47c517c2e530f7c506a6ed5718f5bd8e239a089
Lokesh Basu <lokesh.basu@gmail.com>
2014-06-04 07:35:47 +0530

    sample demo

---
+++ TutorialsPoint/erbdemo.rb
@@ -0,0 +1,10 @@
+<% page_title = "Tutorials Point ERB Demo" %>
+<% message = "Hello, world!" %>
+<html>
+	<head>
+		<title><%= page_title %></title>
+	</head>
+	<body>
+		<p><%= message %></p>
+	</body>
+</html>
